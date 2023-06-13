6174f5b7a0c9e86ae52632466cd5a52cc2c80775
KRcopy <elandyg1@gmail.com>
2023-06-13 10:59:18 -0400

    may have fixed teleporting to places

--- src/main/java/net/mcreator/allfiction/procedure/ProcedureAlibiBlocksplaceteleport.java
+++ src/main/java/net/mcreator/allfiction/procedure/ProcedureAlibiBlocksplaceteleport.java
@@ -9,28 +9,17 @@ public class ProcedureAlibiBlocksplaceteleport extends ElementsAllfiction.ModEle
 	public ProcedureAlibiBlocksplaceteleport(ElementsAllfiction instance) {
 		super(instance, 65);
 	}
-
+	
 	public static void executeProcedure(java.util.HashMap<String, Object> dependencies) {
-		if (dependencies.get("entity") != null) {
+		if (dependencies.get("entity") == null) {
 			System.err.println("Failed to load dependency entity for procedure AlibiBlocksplaceteleport!");
 			return;
 		}
-		if (dependencies.get("x") != null) {
-			System.err.println("Failed to load dependency x for procedure AlibiBlocksplaceteleport!");
-			return;
-		}
-		if (dependencies.get("y") != null) {
-			System.err.println("Failed to load dependency y for procedure AlibiBlocksplaceteleport!");
-			return;
-		}
-		if (dependencies.get("z") != null) {
-			System.err.println("Failed to load dependency z for procedure AlibiBlocksplaceteleport!");
-			return;
-		}
-		Entity entity = (Entity) dependencies.get("entity");
-		int x = (int) dependencies.get("x");
-		int y = (int) dependencies.get("y");
-		int z = (int) dependencies.get("z");
+		int x = dependencies.get("x");
+		int y = dependencies.get("y");
+		int z = dependencies.get("z");
+		print (x,y,z);
+		
 		entity.setPositionAndUpdate(x, y, z);
 	}
 }
