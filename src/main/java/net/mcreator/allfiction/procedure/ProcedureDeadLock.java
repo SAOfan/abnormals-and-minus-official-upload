2dff5969a222ec6720914b2acad04e8e77a313e7
KRcopy <elandyg1@gmail.com>
2023-06-20 10:57:45 -0400

    added comment to dead lock

--- src/main/java/net/mcreator/allfiction/procedure/ProcedureDeadLock.java
+++ src/main/java/net/mcreator/allfiction/procedure/ProcedureDeadLock.java
@@ -73,3 +73,4 @@ public class ProcedureDeadLock extends ElementsAllfiction.ModElement {
 		MinecraftForge.EVENT_BUS.register(this);
 	}
 }
+//the only way to die is something taht dose over 20 damage in 1 go but it can be over come with armour sadly(they can live in the void)
